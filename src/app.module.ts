5716c855667e146627ec100563ad2aa523dcdbed
sebamomann <github@sebamomann.de>
2020-12-19 11:04:40 +0100

    typeorm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { LinkController } from './link/link.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
+            timezone: '+02:00',
+            entities: [],
+            synchronize: true
+        })
+    ],
+    controllers: [AppController, LinkController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
